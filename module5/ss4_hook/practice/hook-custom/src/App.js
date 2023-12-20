a7b3894459e51d1a84f79881db245507f9e66bcc
ThanhVViet <destielisdestiny1111@gmail.com>
2023-12-20 14:38:54 +0700

    ..

---
+++ module5/ss4_hook/practice/hook-custom/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import MyClock from "./components/MyClock";
+
+
+
+function App() {
+  return (
+      <div class="Container">
+        <MyClock />
+      </div>
+  );
+}
+
+export default App;
--- module5/ss4_hook/practice/usestate/src/components/Counter.js
+++ module5/ss4_hook/practice/usestate/src/components/Counter.js
@@ -3,4 +3,19 @@ import {useState} from "react";
 
 function Counter(){
     let [count, setCount] = useState(0);
+
+    const handleClick = () => {
+        const newValue = count + 1;
+        setCount(newValue);
+    };
+    return (
+        <div>
+            Giá trị {count}
+            <div>
+                <button onClick={handleClick}>Up</button>
+            </div>
+        </div>
+    )
 }
+
+export default Counter;
